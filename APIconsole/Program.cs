df935ebcfd2dbb02bc6568bd53080dd319231e92
Марина Никонорова <89656135678@mail.ru>
2021-06-21 13:14:53 +0300

    Bot telega

--- APIconsole/Program.cs
+++ APIconsole/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.IO;
-using System.Net;
+using Telegram.Bot;
 using Newtonsoft.Json;
+using System.Net;
 
-namespace APIconsole
+namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            TelegramBotClient bot = new TelegramBotClient("1834940961:AAH8Weyp7Ai43AufR97UFFspTEp7aYDTUSw");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, Name());
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        static string Name()
+        {
+
             var ApiKey = "ccd07fd87a54f4899fb3778c88570e58";
             var character = "rick";
-            var url = $"https://rickandmortyapi.com/api/character?appid=ccd07fd87a54f4899fb3778c88570e58&q={character}";
+            var url = $"https://rickandmortyapi.com/api/character?appid=ccd07fd87a54f..{character}";
 
             var request = WebRequest.Create(url);
 
@@ -21,17 +39,17 @@ namespace APIconsole
             if (httpStatusCode != HttpStatusCode.OK)
             {
                 Console.WriteLine(httpStatusCode);
-                return;
+                //return "null";
             }
 
             using (var streamReader = new StreamReader(response.GetResponseStream()))
             {
                 string result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
+                //Console.WriteLine(result);
                 var data = JsonConvert.DeserializeObject<Root>(result);
+                return $"{data.results[5].image}";
                 //Console.WriteLine();
             }
-
         }
     }
 }
